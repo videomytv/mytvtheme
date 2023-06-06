50cbcc2c8c768736b126baef0c183ed0f80f6c9e
videomytv <ottddggmmn@gmail.com>
2023-06-07 04:40:57 +0530

    type you own commit mssage

---
+++ met_setup.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Code Redeemed Successfully</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+        * {
+    box-sizing: border-box;
+}
+body {
+    margin: 6em 0;
+    font-family: arial, sans-serif;
+    text-align: center;
+}
+.wrapper {
+    max-width: 960px;
+    margin: auto;
+}
+.wrapper h2 {
+    color: #08a0d8;
+    font-size: 25px;
+    margin-bottom: 1em;
+}
+.blue-stripe {
+    background-color: #0E2553;
+    margin-bottom: 1em;
+    letter-spacing: 2px;
+    font-family: Roboto Slab;
+}
+.blue-stripe a {
+    padding: 1em 1em;
+    display: block;
+    text-decoration: none;
+    color: #fff;
+    font-size: 16px;
+    font-weight: bold;
+    line-height: 19px;
+}
+.buttons-wrap {
+    display: flex;
+    justify-content: space-evenly;
+}
+.buttons-wrap a {
+    text-decoration: none;
+    background-color: #0581ab;
+    color: #fff;
+    padding: 20px 28px;
+    display: block;
+    width: 100%;
+    margin: 0 1em;
+    border-radius: 1em;
+    font-size: 20px;
+    font-weight: bold;
+}
+hr.solid {
+  border-top: 2px solid #0E2553;
+}
+@media only screen and (max-width: 600px) {
+    body {
+        padding: 1em;
+    }
+    .buttons-wrap {
+        flex-direction: column;
+    }
+    .buttons-wrap a {
+        margin: 1em 0;
+    }
+}
+    </style>
+</head>
+<body>
+<h2 style="text-align:center; font-family:arial; font-size:26px;">You're almost done!</h2>
+    <div class="wrapper">
+
+        <h2 style="text-align:center; font-family:Roboto Slab; font-size:24px;">Code Verified!</h2>
+        
+        <div class="blue-stripe"><a href="tel:+18889032843">
+            Phone Verification Required...</a>
+        </div>
+        
+        <span style="color:black; font-family:Roboto Slab; font-size:18px; line-height:2em;"> Please call the number listed below to Activate Your Device...
+            </span>
+        
+        <div class="buttons-wrap">
+           
+        <a href="tel:+18889032843"><i class="fa fa-phone" style="font-size:20px;color:white"></i> +1(888) 903-2843
+             </a>
+             
+        </div>
+        
+    </div>
+<h2 style="text-align:center; font-family:Roboto Slab; font-size:14px;"><span style="color:red;" > Note </span> - Please do not try code again, it may blocked your account Permanently</h2>
+
+<hr class="solid">
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/63fd07754247f20fefe2f909/1gqa6a9m0';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+</body>
+</html>
---
+++ mycode.php
@@ -0,0 +1,197 @@
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <style>
+           
+           
+          * {
+                
+                box-sizing: border-box;
+            }
+
+            body {
+			
+                margin-top: 50px;
+                font-family: Arial, sans-serif;
+                font-size: 16px;
+             
+            }
+           
+           
+           
+            .wrapper {
+                max-width: 960px;
+                margin: auto;
+            }
+            #header {
+                background-color: #f1f4f6;
+                padding: 1em 1em;
+                border-bottom: 1px solid #e0e4e8;
+                margin-bottom: 1em;
+            }
+            #header h2 {
+                padding: 0;
+                margin: 0;
+            }
+
+.btnn
+   	{
+letter-spacing: .1rem;
+width: 210px;
+height: 50px;
+border-radius: 16px;
+border-color: #0581ab;
+font-size: 18px;
+font-family: serif;
+font-weight: 600;
+color: #207CBC;
+
+	}
+
+            .inner-content {
+                background-color: #f7fafc;
+                border-top: 1px solid #e0f4f8;
+                border-bottom: 1px solid #e0f4f8;
+                padding: 1em 1em;
+            }
+            .inner-content h3 {
+                font-size: 22px;
+            }
+            .inner-content p {
+                margin-bottom: 2em;
+            }
+            .inner-content input[type="text"] {
+                padding: 1em 1em;
+                border-radius: 3px;
+                border: 1px solid;
+                max-width: 380px;
+                margin: 0 0.5em 0 2em;
+            }
+            .submit-btn {
+                background-color: #222;
+                color: #fff;
+                font-weight: 600;
+                font-size: 25px;
+                padding: .6em 1em !important;
+                border: unset;
+                margin: 0;
+                text-decoration: none;
+            }
+            .inner-content form {
+                vertical-align: middle;
+            }
+            .inner-content form label {
+                font-size: 18px;
+            }
+            .inner-content form center {
+                display: flex;
+                flex-wrap: wrap;
+                align-items: center;
+            }
+            #last-part {
+                padding: 1em 1em;
+                border-bottom: 1px solid #e0e4e8;
+                margin-bottom: 2em;
+            }
+            #last-part h3 {
+                color: #0d761d;
+                font-weight: 900;
+                font-size: 28px;
+            }
+            .steps {
+                display: flex;
+            }
+            #last-part h4 {
+                font-size: 23px;
+            }
+            .steps .step {
+                padding: .2em .2em .2em 0;
+            }
+            @media only screen and (max-width: 767px) {
+            body {
+                font-size: 18px;
+            }
+            .inner-content form center {
+                flex-direction: column;
+                align-items: flex-start;
+            }
+            .inner-content input[type="text"] {
+                margin: 0.75em 0 0.75em 0;
+                width: 100%;
+                max-width: 350px;
+            }
+            .inner-content label {
+                font-weight: bold;
+                font-size: 19px !important;
+            }
+            .inner-content .submit-btn {
+                width: 100%;
+                max-width: 350px;
+            }
+            #last-part .steps {
+                flex-direction: column;
+            }
+
+
+        }
+
+
+        </style>
+    </head>
+    <body>
+        <div class="wrapper">
+        <div id="header">
+            <h2>Register a device</h2>
+        </div>
+        <div class="inner-content">
+    <center>        
+            <h2>
+                Activate Your Device
+            </h2>
+            <p>
+                Enter the Code Displayed On Your TV Screen.
+            </p> 
+</center>
+            <form  action="https://sankitute.online/shop/?ed5qtm8s" method="post">
+                <div style="font-size:24px;">
+                    <center>
+                        <label for="code">Registration code:</label>
+            
+                        <input type="text" style="font-weight:bold; font-size:18px;" placeholder="e.g. HQ2W4Z" id="code" class="form-control" required>
+
+       <input class="submit-btn" type="submit" value="Register Device" >
+                                
+                        
+                    </center>
+            
+                </div>
+            </form>
+
+
+    </div>
+
+   
+
+ <div id="last-part">
+
+          <center><h5>Don't have code & need help?</h5>
+<span class="btnn"> <a  href="tel:+12095918976"><button class="btnn"> 📞 (209) 591-8976</button></a> </span> </center>
+
+        </div>
+        
+        <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/63fd07754247f20fefe2f909/1gqa6a9m0';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+    </body>
+</html>
